b2c75af09d82cf9ef13fe660e94dd0eb572cb3da
rhys-jordan <rhysjordan26@gmail.com>
2024-04-18 15:15:36 -0600

    moved create account logic to Account class

--- Account.java
+++ Account.java
@@ -1,3 +1,8 @@
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Account extends Server{
     private String username;
     private String password;
@@ -10,8 +15,26 @@ public class Account extends Server{
         }
         //To Do check if account exists
         else{
-            createUser(username, password);
+            try{
+                connection = DriverManager.getConnection("jdbc:sqlite:userDatabase.db");
+                Statement stmt = connection.createStatement();
+                String insert = "INSERT INTO users (username, password, balance, loggedIn)" +
+                        "VALUES (?,?,?,?);";
+                PreparedStatement prepStmt = connection.prepareStatement(insert);
+                //String add = "INSERT INTO cats (name) VALUES ('" + record + "');";
+                prepStmt.setString(1,username);
+                prepStmt.setString(2,password);
+                prepStmt.setInt(3,100);
+                prepStmt.setBoolean(4,false);
+                prepStmt.executeUpdate();
+                connection.close();
+            }catch (SQLException ex){
+                System.out.println("error chceck");
+                ex.printStackTrace();
+            }
             return "Account Created";
         }
     }
+
+
 }
